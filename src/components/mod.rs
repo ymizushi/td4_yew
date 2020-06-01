ab12c0a1c4a3da7a41e470f90eef44f031137f12
ymizushi <mizushi@gmail.com>
2020-06-02 03:43:33 +0900

    add registers

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,12 +1,41 @@
 use yew::prelude::*;
+use std::fmt;
+
+#[allow(non_camel_case_types)]
+#[derive(Clone, Debug, Copy)]
+pub struct u4 {
+    v: u8
+}
+
+impl u4 {
+    pub fn add(&mut self, v: u4) -> (bool, u4) {
+        self.v += v.v;
+        if self.v >= 16 {
+            self.v -= 15;
+            (true, *self)
+        } else {
+            (false, *self)
+        }
+    }
+}
+
+impl fmt::Display for u4 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.v)
+    }
+}
 
 pub struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    register_a: u4,
+    register_b: u4,
+    carry: bool,
+    pc: u4,
 }
 
 pub enum Msg {
-    AddOne,
+    Clock,
+    Reset,
 }
 
 impl Component for Model {
@@ -15,13 +44,21 @@ impl Component for Model {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: 0,
+            register_a: u4 {v: 0},
+            register_b: u4 {v: 0},
+            carry: false,
+            pc: u4 {v: 0},
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1
+            Msg::Clock => {
+                self.register_a.add(u4 { v: 2 });
+                self.pc.add(u4 {v: 1});
+            }
+            _ => {
+            }
         }
         true
     }
@@ -36,8 +73,10 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <div>{ "Register A" } { self.register_a }</div>
+                <div>{ "Register B" } { self.register_b }</div>
+
+                <button onclick=self.link.callback(|_| Msg::Clock)>{ "Clock" }</button>
             </div>
         }
     }

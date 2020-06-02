93c1fb6d9e5e4c78c5cbd61f2112b63f21cf8e2c
ymizushi <mizushi@gmail.com>
2020-06-02 15:48:12 +0900

    fix warning

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -8,13 +8,13 @@ pub struct u4 {
 }
 
 impl u4 {
-    pub fn add(&mut self, v: u4) -> (bool, u4) {
+    pub fn add(&mut self, v: u4) -> bool {
         self.v += v.v;
         if self.v >= 16 {
             self.v -= 16;
-            (true, *self)
+            true
         } else {
-            (false, *self)
+            false
         }
     }
 }
@@ -39,38 +39,103 @@ pub struct Model {
 pub enum Msg {
     Clock,
     Reset,
-    ROMOperation(u4),
     GotInput(usize, String),
 }
 
 impl Model {
     fn clock(&mut self) {
-        let (carry, pc) = self.pc.add(u4 {v: 1});
-        self.carry = carry; // あとでキャリーフラグのクリア処理を書く
-        self.operate(self.rom[pc.v as usize]);
+        self.operate(self.rom[self.pc.v as usize]);
+        self.pc.add(u4 {v: 1});
+    }
+
+    fn reset(&mut self) {
+        self.register_a= u4 {v: 0};
+        self.register_b= u4 {v: 0};
+        self.carry= false;
+        self.pc= u4 {v: 0};
+        self.o_port= u4 {v: 0};
+        self.i_port= u4 {v: 0};
+        self.rom= [0; 16];
     }
 
     fn operate(&mut self, r: u8) {
         let opcode  = r >> 4;
-        let operand = r & 0b00001111u8;
+        let im = r & 0b00001111u8;
         match opcode {
-            0b0000 => self.register_a.add(u4 {v: opcode}),
-            0b0101 => self.register_b.add(u4 {v: opcode}),
-            _ => {(true, u4 {v: 0})}
+            0b0011 => { // MOV A, Im
+                self.register_a.v = im;
+                self.carry = false;
+            }
+            0b0111 => { // MOV B, Im
+                self.register_b.v = im;
+                self.carry = false;
+            }
+            0b0001 => { // MOV A, B
+                self.register_a.v = self.register_b.v;
+                self.carry = false;
+            }
+            0b0100 => { // MOV B, A
+                self.register_b.v = self.register_a.v;
+                self.carry = false;
+            }
+            0b0000 => { // ADD A, Im
+                self.carry = self.register_a.add(u4 {v: im});
+            },
+            0b0101 => { // ADD B, Im
+                self.carry = self.register_b.add(u4 {v: im});
+            },
+            0b0010 => { // IN A
+                self.register_a.v = self.i_port.v;
+                self.carry = false;
+            },
+            0b0110 => { // IN B
+                self.register_b.v = self.i_port.v;
+                self.carry = false;
+            },
+            0b1011 => { // OUT Im
+                self.o_port.v = im;
+                self.carry = false;
+            },
+            0b1001 => { // OUT B
+                self.o_port.v = self.register_b.v;
+                self.carry = false;
+            },
+            0b1111 => { // JMP Im
+                self.pc.v = im;
+                self.carry = false;
+            },
+            0b1110 => { // JNC Im
+                if !self.carry {
+                    self.pc.v = im;
+                } 
+                self.carry = false;
+            },
+            _ => panic!("Unknown opcode")
         };
     }
 }
 
 impl Model {
-    fn view_rom(&self, index: usize, rom: u8) -> Html {
+    fn view_rom_item(&self, index: usize, rom: u8) -> Html {
         html! {
-            <div>
-                <p>{"index: "}{ index }</p>
-                <p>{"rom: "}{ rom }</p>
-                <textarea rows=5
+            <div class="td4-rom-item">
+                <div>{"index: "}{ index }</div>
+                <textarea rows=1
                     value=rom
                     oninput=self.link.callback(move |e: InputData| Msg::GotInput(index, e.value))
                     placeholder="placeholder" />
+            </div>
+        }
+    }
+    fn view_registers(&self) -> Html {
+        html! {
+            <div class="td4-registers">
+                <div class="td4-registers-item">{ "Register A: " } { self.register_a }</div>
+                <div class="td4-registers-item">{ "Register B: " } { self.register_b }</div>
+                <div class="td4-registers-item">{ "Carry: " } { self.carry }</div>
+                <div class="td4-registers-item">{ "PC: " } { self.pc }</div>
+                <div class="td4-registers-item">{ "o_port: " } { self.o_port }</div>
+                <div class="td4-registers-item">{ "i_port: " } { self.i_port }</div>
             </div>
         }
     }
@@ -89,7 +154,7 @@ impl Component for Model {
             pc: u4 {v: 0},
             o_port: u4 {v: 0},
             i_port: u4 {v: 0},
-            rom: [0; 16],
+            rom: [4; 16],
         }
     }
 
@@ -99,11 +164,17 @@ impl Component for Model {
                 self.clock();
             }
             Msg::Reset => {
-                self.pc = u4 {v: 0};
-            }
-            Msg::ROMOperation(_) => {
+                self.reset();
             }
             Msg::GotInput(index, rom) => {
+                match rom.parse() {
+                    Ok(rom) => {
+                        self.rom[index] = rom;
+                    }
+                    Err(e) => {
+                        println!("{}: string is allowed to 0-15", e);
+                    }
+                }
             }
         }
         true
@@ -119,12 +190,13 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <div>{ "Register A: " } { self.register_a }</div>
-                <div>{ "Register B: " } { self.register_b }</div>
-                <div>{ "PC: " } { self.pc }</div>
-                { self.rom.into_iter().enumerate().map(|(i, rom)| self.view_rom(i, *rom)).collect::<Html>() }
+            <div class="td4">
+                { self.view_registers() }
+                <div class="td4-rom">
+                    { self.rom.iter().enumerate().map(|(i, rom)| self.view_rom_item(i, *rom)).collect::<Html>() }
+                </div>
                 <button onclick=self.link.callback(|_| Msg::Clock)>{ "Clock" }</button>
+                <button onclick=self.link.callback(|_| Msg::Reset)>{ "Reset" }</button>
             </div>
         }
     }

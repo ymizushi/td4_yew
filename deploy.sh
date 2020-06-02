a2821d80ce2b4100ad6de04b5582a1b618012631
ymizushi <mizushi@gmail.com>
2020-06-02 18:27:44 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -a static/ ../ymizushi.github.io/td4-yew/
+rsync -a static/ ../ymizushi.github.io/td4-yew/ --exclude .gitignore
 cd ../ymizushi.github.io/td4-yew/
 git add .
 git commit -m "update content"

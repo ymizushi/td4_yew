9fd150ef6d9cb002b29721267c3ee0e97e945c18
ymizushi <mizushi@gmail.com>
2020-06-02 13:38:45 +0900

    add code

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -11,7 +11,7 @@ impl u4 {
     pub fn add(&mut self, v: u4) -> (bool, u4) {
         self.v += v.v;
         if self.v >= 16 {
-            self.v -= 15;
+            self.v -= 16;
             (true, *self)
         } else {
             (false, *self)
@@ -31,12 +31,51 @@ pub struct Model {
     register_b: u4,
     carry: bool,
     pc: u4,
+    o_port: u4,
+    i_port: u4,
+    rom: [u8;16]
 }
 
 pub enum Msg {
     Clock,
     Reset,
+    ROMOperation(u4),
+    GotInput(usize, String),
 }
+
+impl Model {
+    fn clock(&mut self) {
+        let (carry, pc) = self.pc.add(u4 {v: 1});
+        self.carry = carry; // あとでキャリーフラグのクリア処理を書く
+        self.operate(self.rom[pc.v as usize]);
+    }
+
+    fn operate(&mut self, r: u8) {
+        let opcode  = r >> 4;
+        let operand = r & 0b00001111u8;
+        match opcode {
+            0b0000 => self.register_a.add(u4 {v: opcode}),
+            0b0101 => self.register_b.add(u4 {v: opcode}),
+            _ => {(true, u4 {v: 0})}
+        };
+    }
+}
+
+impl Model {
+    fn view_rom(&self, index: usize, rom: u8) -> Html {
+        html! {
+            <div>
+                <p>{"index: "}{ index }</p>
+                <p>{"rom: "}{ rom }</p>
+                <textarea rows=5
+                    value=rom
+                    oninput=self.link.callback(move |e: InputData| Msg::GotInput(index, e.value))
+                    placeholder="placeholder" />
+            </div>
+        }
+    }
+}
+
 
 impl Component for Model {
     type Message = Msg;
@@ -48,16 +87,23 @@ impl Component for Model {
             register_b: u4 {v: 0},
             carry: false,
             pc: u4 {v: 0},
+            o_port: u4 {v: 0},
+            i_port: u4 {v: 0},
+            rom: [0; 16],
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Clock => {
-                self.register_a.add(u4 { v: 2 });
-                self.pc.add(u4 {v: 1});
+                self.clock();
             }
-            _ => {
+            Msg::Reset => {
+                self.pc = u4 {v: 0};
+            }
+            Msg::ROMOperation(_) => {
+            }
+            Msg::GotInput(index, rom) => {
             }
         }
         true
@@ -70,12 +116,14 @@ impl Component for Model {
         false
     }
 
+
     fn view(&self) -> Html {
         html! {
             <div>
-                <div>{ "Register A" } { self.register_a }</div>
-                <div>{ "Register B" } { self.register_b }</div>
-
+                <div>{ "Register A: " } { self.register_a }</div>
+                <div>{ "Register B: " } { self.register_b }</div>
+                <div>{ "PC: " } { self.pc }</div>
+                { self.rom.into_iter().enumerate().map(|(i, rom)| self.view_rom(i, *rom)).collect::<Html>() }
                 <button onclick=self.link.callback(|_| Msg::Clock)>{ "Clock" }</button>
             </div>
         }
